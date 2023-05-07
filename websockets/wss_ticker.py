e3f8e83ee2cd4b1c89c305ac5df31a078c73acb8
ahmetax <ahmetax@axtelsoft.com>
2023-05-07 12:25:27 +0300

    wss_ticker.py eklendi

---
+++ websockets/wss_ticker.py
@@ -0,0 +1,44 @@
+import websocket
+import json
+import time
+
+def on_open(ws):
+    print("on_open")
+
+    subscribe={
+        "c":"ticker@ethusdt",
+        "s":True
+    }
+
+    ws.send("subscribe|"+json.dumps(subscribe))
+
+def on_message(ws, message):
+    # print('on_message')
+    if not message.startswith("ticker|"):
+        return
+    message=message.split('|')[1]
+    r = json.loads(message)
+    # pair, price, volume, 24hr change
+    print(f"{r['ps']:10} {float(r['p']):18.10f} {float(r['v']):15.4f} {float(r['cp']):8.2f}%")
+    time.sleep(2)
+    # ws.close()
+
+def on_error(ws, error):
+    print("on_error")
+    print(error)
+
+def on_close(ws, close_status_code, close_msg):
+    print("on_close")
+
+
+if __name__ == "__main__":
+    url = "wss://istream.icrypex.com"
+    # websocket.enableTrace(True)
+    ws = websocket.WebSocketApp(
+        url,
+        on_open=on_open,
+        on_message=on_message,
+        # on_error=on_error,
+        on_close=on_close,
+    )
+    ws.run_forever()
